26dfd4ded0ddf395edc1e8f5322c9f14ff57d244
bobw@prophecy.io <bobw@prophecy.io>
2025-03-06 14:21:55 +0000

    Refactors demo_new_sql model to enhance customer sales insights and limit results to top 25 sales.

--- models/demo_new_sql.sql
+++ models/demo_new_sql.sql
@@ -5,45 +5,115 @@
   })
 }}
 
-WITH bobw_helloworld_hw_customers AS (
+WITH SalesModel AS (
 
+  WITH HW_ORDERS AS (
+  
+    SELECT * 
+    
+    FROM {{ source('BOBW.HELLOWORLD', 'HW_ORDERS') }}
+  
+  ),
+  
+  HW_CUSTOMERS AS (
+  
+    SELECT * 
+    
+    FROM {{ source('BOBW.HELLOWORLD', 'HW_CUSTOMERS') }}
+  
+  ),
+  
+  customer_order_details AS (
+  
+    {#Compiles detailed customer information alongside their order history for better customer insights.#}
+    SELECT 
+      in1.CUSTOMER_ID AS CUSTOMER_ID,
+      in1.FIRST_NAME AS FIRST_NAME,
+      in1.LAST_NAME AS LAST_NAME,
+      in1.PHONE AS PHONE,
+      in1.EMAIL AS EMAIL,
+      in1.COUNTRY_CODE AS COUNTRY_CODE,
+      in1.ACCOUNT_OPEN_DATE AS ACCOUNT_OPEN_DATE,
+      in1.ACCOUNT_FLAGS AS ACCOUNT_FLAGS,
+      in0.ORDER_ID AS ORDER_ID,
+      in0.AMOUNT AS AMOUNT,
+      in0.ORDER_STATUS AS ORDER_STATUS,
+      in0.ORDER_CATEGORY AS ORDER_CATEGORY,
+      in0.ORDER_DATE AS ORDER_DATE
+    
+    FROM HW_ORDERS AS in0
+    INNER JOIN HW_CUSTOMERS AS in1
+       ON in0.CUSTOMER_ID = in1.CUSTOMER_ID
+  
+  )
+  
   SELECT * 
   
-  FROM {{ source('BOBW.HELLOWORLD', 'HW_CUSTOMERS') }}
+  FROM customer_order_details
 
 ),
 
-bobw_helloworld_hw_orders AS (
+sales_customer_details AS (
 
-  SELECT * 
+  {#Compiles customer sales data, including full names and company domains from email addresses.#}
+  SELECT 
+    CUSTOMER_ID AS CUSTOMER_ID,
+    CONCAT(FIRST_NAME, ' ', LAST_NAME) AS full_name,
+    ORDER_ID AS ORDER_ID,
+    AMOUNT AS AMOUNT,
+    SPLIT(EMAIL, '@')[1] AS company_name
   
-  FROM {{ source('BOBW.HELLOWORLD', 'HW_ORDERS') }}
+  FROM SalesModel
 
 ),
 
-order_customer_details AS (
+sales_summary_by_customer AS (
 
   SELECT 
-    bobw_helloworld_hw_orders.ORDER_ID,
-    bobw_helloworld_hw_orders.CUSTOMER_ID,
-    bobw_helloworld_hw_orders.ORDER_STATUS,
-    bobw_helloworld_hw_orders.ORDER_CATEGORY,
-    bobw_helloworld_hw_orders.ORDER_DATE,
-    bobw_helloworld_hw_orders.AMOUNT,
-    bobw_helloworld_hw_customers.FIRST_NAME,
-    bobw_helloworld_hw_customers.LAST_NAME,
-    bobw_helloworld_hw_customers.PHONE,
-    bobw_helloworld_hw_customers.EMAIL,
-    bobw_helloworld_hw_customers.COUNTRY_CODE,
-    bobw_helloworld_hw_customers.ACCOUNT_OPEN_DATE,
-    bobw_helloworld_hw_customers.ACCOUNT_FLAGS
+    FULL_NAME,
+    COMPANY_NAME,
+    SUM(AMOUNT) AS SUM_AMOUNT,
+    COUNT(ORDER_ID) AS ORDER_COUNT
   
-  FROM bobw_helloworld_hw_orders
-  INNER JOIN bobw_helloworld_hw_customers
-     ON bobw_helloworld_hw_orders.CUSTOMER_ID = bobw_helloworld_hw_customers.CUSTOMER_ID
+  FROM sales_customer_details
+  
+  GROUP BY 
+    FULL_NAME, COMPANY_NAME
+
+),
+
+total_sales_summary AS (
+
+  SELECT 
+    FULL_NAME,
+    COMPANY_NAME,
+    ROUND(SUM_AMOUNT, 2) AS TOTAL_SALES,
+    ORDER_COUNT
+  
+  FROM sales_summary_by_customer
+
+),
+
+total_sales_desc AS (
+
+  SELECT * 
+  
+  FROM total_sales_summary
+  
+  ORDER BY TOTAL_SALES DESC
+
+),
+
+limited_rows AS (
+
+  SELECT * 
+  
+  FROM total_sales_desc
+  
+  FETCH NEXT 25 ROWS ONLY
 
 )
 
 SELECT *
 
-FROM order_customer_details
+FROM limited_rows
